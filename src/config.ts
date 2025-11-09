b08ed92b82a9e2c84610be6a1343429984d4e205
eyeol <daniel4906@korea.ac.kr>
2025-11-10 01:52:49 +0900

    bio 수정

--- src/config.ts
+++ src/config.ts
@@ -55,7 +55,7 @@ export const navBarConfig: NavBarConfig = {
 export const profileConfig: ProfileConfig = {
 	avatar: "assets/images/avatar.png", // Relative to the /src directory. Relative to the /public directory if it starts with '/'
 	name: "eyeol",
-	bio: "A New Day with Hope",
+	bio: "Bruised lungs, bright breath.",
 	links: [
 		{
 			name: "GitHub",
