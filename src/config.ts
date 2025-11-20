6752f656ccb4f5256bf6faeb1fa958bf7b946875
eyeol <daniel4906@korea.ac.kr>
2025-11-21 03:47:03 +0900

    앞으로 취준할 때까지의 다짐

--- src/config.ts
+++ src/config.ts
@@ -55,7 +55,7 @@ export const navBarConfig: NavBarConfig = {
 export const profileConfig: ProfileConfig = {
 	avatar: "assets/images/avatar.png", // Relative to the /src directory. Relative to the /public directory if it starts with '/'
 	name: "eyeol",
-	bio: "Bruised lungs, bright breath.",
+	bio: "Routines over moods.",
 	links: [
 		{
 			name: "GitHub",
